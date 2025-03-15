36c7d8981c63f57af358836ec1fe243f6a04de0e
Ali1-coder <ablhur3@gmail.com>
2025-03-15 22:57:17 +0300

    Table Bears created and querried successfully

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+    ('Mr. Chocolate', 20, 'M', 'Brown', 'Calm', 1),
+    ('Rowdy', 10, 'M', 'Black', 'Aggressive', 1),
+    ('Tabitha', 6, 'F', 'Golden', 'Gentle', 1),
+    ('Sergeant Brown', 14, 'M', 'Dark Brown', 'Dominant', 0),
+    ('Melissa', 13, 'F', 'White', 'Friendly', 1),
+    ('Grinch', 2, 'M', 'Greenish', 'Grumpy', 1),
+    ('Wendy', 6, 'F', 'Brown', 'Calm', 1),
+    (NULL, 7, 'M', 'Gray', 'Mysterious', 0);
